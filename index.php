69089002ef103d6d6c8471fea60d985e44874c73
Quill McConnell <quill8197@yahoo.com>
2020-05-01 16:54:45 -0400

    Added form validation to the order page and fixed the header on the order page

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 
 //Turn on error reporting
 ini_set('display_errors', 1);
@@ -11,14 +12,40 @@ require_once('vendor/autoload.php');
 $f3 = Base::instance();   //a Fat Free object
 
 //Define a default route
-$f3->route('GET /', function() {
+$f3->route('GET /', function()
+{
     $view = new Template();
     echo $view->render('views/pet-home.html');
 });
 
 //Define an order route
-$f3->route('GET /order', function() {
-    echo '<h1>Order Page</h1>';
+$f3->route('GET|POST /order', function($f3)
+{
+    //Check if the form has been posted
+    if ($_SERVER['REQUEST_METHOD'] == 'POST')
+    {
+        //Validate the data
+        if (empty($_POST['pet']))
+        {
+            //Data is invalid
+            echo 'Please supply a pet type';
+        }
+        else
+        {
+            //Data is valid
+            $_SESSION['pet'] = $_POST['pet'];
+
+            //Add color to the session
+            $_SESSION['color'] = $_POST['color'];
+
+            //Redirect to the summary route
+            $f3->reroute("summary");
+        }
+    }
+    else
+    {
+        echo 'get method';
+    }
 
     $view = new Template();
     echo $view->render('views/pet-order.html');
